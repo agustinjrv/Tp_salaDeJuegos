785c62b2c3ccf1c1b5c03fde194664b91c7cdb76
agustinrv <agustinrivola2000@gmail.com>
2021-03-30 18:04:38 -0300

    a

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -27,7 +27,6 @@ import { LoginComponent } from './pages/login/login.component';
     AppRoutingModule,
     FormsModule,
     AngularFireModule.initializeApp(environment.firebaseConfig)
-    
   ],
   providers: [],
   bootstrap: [AppComponent]
